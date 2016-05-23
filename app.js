33e30ae61d8ed8ec87421b7ef705658c251beba3
Adrien Dubois <adrien.dubois.be@gmail.com>
2016-05-23 21:35:29 +0200

    Reading data from response

--- app.js
+++ app.js
@@ -8,15 +8,24 @@ function printMessage(username, badgeCount, points){
   console.log(message);
 }
 
-// Connet to the API URL --> https://teamtreehouse.com/adriendubois.json
-  var request = https.get("teamtreehouse.com/adriendubois.json", function(response){
-    console.dir(response);
+// Connect to the API URL --> https://teamtreehouse.com/adriendubois.json
+var request = https.get("https://teamtreehouse.com/adriendubois.json", function(response){
+  var body = '';
+
+// Read the data
+  response.on('data', function(chunk){
+    body += chunk;
+  });
+  response.on('end', function(){
+    console.log(body);
   });
 
-  request.on('error', function(error){
-    console.error(error.message);
-  });
-
-// Read the data from the response
 // Parse the data
 // Print the data
+
+});
+
+request.on('error', function(error){
+  console.error(error.message);
+});
+
